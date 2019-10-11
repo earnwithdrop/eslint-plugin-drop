96bdfddfbe261757e8bf0b63c828d6dd83d134a1
George Jose <george@earnwithdrop.com>
2019-10-11 16:10:15 -0400

    Update thing

--- tests/lib/rules/jest-return-expect-saga.js
+++ tests/lib/rules/jest-return-expect-saga.js
@@ -2,63 +2,97 @@
  * @fileoverview Require explicit booleans in JSX conditional rendering using &&
  * @author drop
  */
-'use strict';
+'use strict'
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
-const rule = require('../../../lib/rules/jest-return-expect-saga');
+const rule = require('../../../lib/rules/jest-return-expect-saga')
 // eslint-disable-next-line
-const { RuleTester } = require('eslint');
+const { RuleTester } = require('eslint')
 RuleTester.setDefaultConfig({
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: 'module',
-        ecmaFeatures: {
-            modules: true,
-            jsx: true,
-        },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+      jsx: true,
     },
-});
+  },
+})
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester()
 ruleTester.run('jest-return-expect-saga', rule, {
-    valid: [
-        {
-            code: `
-        describe(() => {
-            it(() => {
-                return expectSaga()
+  valid: [
+    {
+      code: `
+            describe(() => {
+                it(() => {
+                    return expectSaga()
+                })
             })
-        })
         `,
-            filename: 'test.js',
-        },
-        {
-            code: `
+      filename: 'test.js',
+    },
+    {
+      code: `
       describe(() => {
         it(() => expectSaga())
       })
         `,
-            filename: 'test.js',
-        },
-    ],
-    invalid: [
+      filename: 'test.js',
+    },
+    {
+      code: `
+        const generateExpectSaga = () => expectSaga()
+          `,
+      filename: 'test.js',
+    },
+    {
+      code: `
+          import { expectSaga } from 'some-library'
+            `,
+      filename: 'test.js',
+    },
+    {
+      code: `
+      it('', () => expectSaga().fn())
+            `,
+      filename: 'test.js',
+    },
+  ],
+  invalid: [
+    {
+      code: `
+            describe(() => {
+                it(() => {
+                    expectSaga()
+                })
+            })
+            `,
+      filename: 'test.js',
+      errors: [
         {
-            code: `
-      describe(() => {
-          it(() => {
-              expectSaga()
-          })
-      })
-      `,
-            filename: 'test.js',
-            errors: [
-                {
-                    message: 'expectSagas in `it` blocks should be returned',
-                },
-            ],
+          message: 'expectSagas in `it` blocks should be returned',
         },
-    ],
-});
+      ],
+    },
+    {
+      code: `
+              describe(() => {
+                  it(() => {
+                      doStuff()
+                      expectSaga()
+                  })
+              })
+              `,
+      filename: 'test.js',
+      errors: [
+        {
+          message: 'expectSagas in `it` blocks should be returned',
+        },
+      ],
+    },
+  ],
+})
