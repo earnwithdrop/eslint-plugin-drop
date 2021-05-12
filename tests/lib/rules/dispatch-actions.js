371e0be35413fba0e4e731ec79c65bc9698c8852
George Jose <george@earnwithdrop.com>
2021-05-12 14:59:48 -0400

    mapDispatchToProps rule

---
+++ tests/lib/rules/dispatch-actions.js
@@ -0,0 +1,65 @@
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const { resolve } = require('path')
+const { TSESLint } = require('@typescript-eslint/experimental-utils')
+const rule = require('../../../lib/rules/dispatch-actions')
+
+const ruleTester = new TSESLint.RuleTester({
+  parser: resolve('./node_modules/@typescript-eslint/parser'),
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+})
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+ruleTester.run('dispatch-actions', rule, {
+  valid: [
+    {
+      code: `
+      const mapDispatchToProps = {
+        action,
+      }
+      `,
+    },
+    {
+      code: `
+        const mapDispatchToProps = {
+          action: actionName,
+        }
+        `,
+    },
+    {
+      code: `
+          const mapDispatchToProps = {
+            action: () => actionName(),
+          }
+          `,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `
+      const mapDispatchToProps = {
+        action: () => {
+            action1()
+            action2()
+        },
+      }
+      `,
+      errors: [
+        {
+          message: 'Did you forget to dispatch actions?',
+        },
+      ],
+    },
+  ],
+})
